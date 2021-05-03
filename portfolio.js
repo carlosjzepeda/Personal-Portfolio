1c4dacafba0461288b7704f7d73e7a767f70d8e1
carlos zepeda <cjzepedaa@gmail.com>
2021-05-03 03:34:14 -0600

    Added Animations and a take me home button
    Added Animations using GSAP to nav bar also added a take me home button at bottom of page and added relevant css classes.

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,54 @@
-init_pointer{{))()}}
-    pointerColor: "#222",
-    ringSize: 15,
-    ringClickSize: 10
-  })
+function currentTime() {
+  let date = new Date();
+  let hour = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  let midDay = (hour >= 12) ? " PM" : "   AM ";
+  hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12) : hour);
+  document.querySelector('#time').innerHTML = hour + ":" + min + ":" + sec + midDay;
+
+  let timeout = setTimeout(() => {
+    currentTime()
+
+  }, 1000);
+}
+
+function updateTime(time) {
+  if (time < 10) {
+    return '0' + time
+  } else {
+    return time
+  }
+}
+currentTime();
+
+
+//scroll to top button
+
+let button = document.querySelector('.home-btn')
+
+button.addEventListener('click', () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0;
+})
+//GSAP ANIMATIONS
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.navtext', {
+  duration: 1,
+  opacity: 0,
+  y: 50,
+  ease: "back",
+  stagger: .05
+});
+
+gsap.from('.about-page', {
+  x: -100,
+  opacity: 0,
+  duration: 1,
+  ease: "back",
+})
