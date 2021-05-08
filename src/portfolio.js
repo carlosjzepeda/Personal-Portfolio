eea08dd999e2e126e747bbd525401c7ab9712539
carlos zepeda <cjzepedaa@gmail.com>
2021-05-08 02:27:54 -0600

    Changed directory of files

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,56 +7,54 @@ function currentTime() {
   hour = updateTime(hour);
   min = updateTime(min);
   sec = updateTime(sec);
-  let midDay = (hour >= 12) ? " PM" : "   AM ";
-  hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12) : hour);
-  document.querySelector('#time').innerHTML = hour + ":" + min + ":" + sec + midDay;
+  let midDay = hour >= 12 ? " PM" : "   AM ";
+  hour = hour == 0 ? 12 : hour > 12 ? hour - 12 : hour;
+  document.querySelector("#time").innerHTML =
+    hour + ":" + min + ":" + sec + midDay;
 
   let timeout = setTimeout(() => {
-    currentTime()
-
+    currentTime();
   }, 1000);
 }
 
 function updateTime(time) {
   if (time < 10) {
-    return '0' + time
+    return "0" + time;
   } else {
-    return time
+    return time;
   }
 }
 currentTime();
 
-
 //scroll to top button
 
-let button = document.querySelector('.home-btn')
+let button = document.querySelector(".home-btn");
 
-button.addEventListener('click', () => {
+button.addEventListener("click", () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0;
-})
+});
 //GSAP ANIMATIONS
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from('.navtext', {
+gsap.from(".navtext", {
   duration: 1,
   opacity: 0,
   y: 50,
   ease: "back",
-  stagger: .05
+  stagger: 0.05,
 });
 
-gsap.from('.about-page', {
+gsap.from(".about-page", {
   x: -100,
   opacity: 0,
   duration: 1.5,
   ease: "back",
-})
-
+});
 
 gsap.to(".skill-img", {
-  boxShadow:"5px 5px",
-  yoyo: true, 
+  boxShadow: "5px 5px",
+  yoyo: true,
   repeat: -1,
-  ease: "sine.inOut"
+  ease: "sine.inOut",
 });
